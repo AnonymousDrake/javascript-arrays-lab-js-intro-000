be6458b5e08ad5a90d9d9a1411a0b240c9170145
Ekambeer Singh <akashdeep0406@gmail.com>
2020-07-09 12:40:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,15 @@
 var kittens = ["Milo", "Otis", "Garfield"];
-var destructivelyAppendKitten = (array,name) =>{
-  array.push(name);
+var destructivelyAppendKitten = (kittens,name) =>{
+  kittens.push(name);
+  return kittens;
+}
+var destructivelyPrependKitten=(kittens,name) =>{
+  kittens.unshift(name);
+  return kittens;
+}
+var appendKitten = (kittens,name) =>{
+  return kittens=[...kittens, name];
+} 
+var prependKitten = (kittens,name)=>{
+  return (kittens=[name,...kittens];)
 }
